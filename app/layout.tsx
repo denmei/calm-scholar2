eaa03b4f97b74478e544bdcaf4378253c4b04d17
denmei <meisnerden@gmail.com>
2025-09-01 20:08:15 +0000

    hello

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import "@/styles/tokens.css";
+import "../styles/tokens.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Providers from "@/src/providers/Providers";
